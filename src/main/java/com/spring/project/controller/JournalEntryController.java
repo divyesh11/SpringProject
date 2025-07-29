450311488e00d228ea91b7fed498f565549cd88a
divyesh11 <divyeshjivani000@gmail.com>
2025-07-29 12:16:47 +0530

    Add initial application configuration, security setup, and journal management features

--- src/main/java/com/spring/project/controller/JournalEntryController.java
+++ src/main/java/com/spring/project/controller/JournalEntryController.java
@@ -1,8 +1,26 @@
 package com.spring.project.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.project.JournalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
+    private Map<Long, JournalEntity> journalEntityMap = new HashMap<Long,JournalEntity>();
+
+    @GetMapping("/all")
+    public List<JournalEntity> getAll() {
+        return new ArrayList<>(journalEntityMap.values());
+    }
+
+    @PostMapping
+    public void createJournal(@RequestBody JournalEntity journalEntity) {
+        journalEntityMap.put(journalEntity.getId(), journalEntity);
+    }
 }
---
+++ src/main/java/com/spring/project/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.spring.project.security;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        Customizer<CsrfConfigurer<HttpSecurity>> configurer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+                httpSecurityCsrfConfigurer.disable();
+            }
+        };
+        Customizer<SessionManagementConfigurer<HttpSecurity>> sessionManagementConfigurerCustomizer = new Customizer<SessionManagementConfigurer<HttpSecurity>>() {
+
+            @Override
+            public void customize(SessionManagementConfigurer<HttpSecurity> httpSecuritySessionManagementConfigurer) {
+                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            }
+        };
+        return httpSecurity.csrf(configurer)
+                .sessionManagement(sessionManagementConfigurerCustomizer).build();
+    }
+}
